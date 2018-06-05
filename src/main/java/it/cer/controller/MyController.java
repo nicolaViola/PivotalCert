b9a9dc7a53dac2d5e45973d01303ae67f3d0185a
Viola_N <nicola.viola@regione.emilia-romagna.it>
2018-06-05 10:42:37 +0200

    Creato un controller per verificare il funzionamento del progetto

---
+++ src/main/java/it/cer/controller/MyController.java
@@ -0,0 +1,50 @@
+package it.cer.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.portlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/users")
+public class MyController {
+	
+//  http://localhost:8080/l159-web/users/list
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Model model) {
+		System.out.println("DDD!");
+		return "users/list";
+	}
+	
+	
+	//  http://localhost:8080/l159-web/users/test/105
+	@RequestMapping(value = "/test/{\\d*}", method = RequestMethod.GET)
+	public String show(Model model) {
+		System.out.println("DDD! id:");
+		return "users/list";
+	}
+	
+	//  http://localhost:8080/l159-web/users/showUser?userId=105
+	@RequestMapping(value = "/showUser", method = RequestMethod.GET)
+	public String showxx(Model model, @RequestParam(value = "userId", required = true) Long userId) {
+		System.out.println("DDD! id:");
+		return "users/list";
+	}
+	
+	
+	//  http://localhost:8080/l159-web/users/105
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	public ModelAndView showx(Model model, @PathVariable("userId") Long userId) {
+		System.out.println("DDD! id:");
+		//return "test";
+		return new ModelAndView("users/106");
+	}
+	
+	
+
+
+}
