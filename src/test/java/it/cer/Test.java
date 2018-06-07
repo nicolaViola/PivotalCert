baa9bfa0fa5978b7d1921056dfe7d308344a44ad
Viola_N <nicola.viola@regione.emilia-romagna.it>
2018-06-07 09:42:12 +0200

    Aggiunta una classe di test (per vedere se crea l'alberatura)

---
+++ src/test/java/it/cer/Test.java
@@ -0,0 +1,16 @@
+package it.cer;
+
+import static org.junit.Assert.fail;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
